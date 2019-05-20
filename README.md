4ada350706178a84587ed5e93507a4bc47e99b50
DimaKlochkow <dimaklochkow@gmail.com>
2019-05-20 13:03:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-rbvph8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-rbvph8)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+}
+var numbers = prompt("Введите числа через запятую!".split(','));
+var str = numbers.split(',');
+console.log(str);
+var sum = 0;
+for(var i = 0; i < str.length; i++){
+    sum += +str[i] / str.length;
+}
+console.log(sum);
